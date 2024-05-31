b4f59f66a9df06742b86b1b175ce94362fa37cde
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-05-31 12:05:43 +0530

    removed env from github

--- Server/config/cloudinary.js
+++ Server/config/cloudinary.js
@@ -14,20 +14,3 @@ exports.cloudinaryConnect = () => {
     }
 }
 
-
-// MAIL_HOST = smtp.gmail.com
-// MAIL_USER = divyanshu30001@gmail.com
-// MAIL_PASS = wseqrxxhkvzodyti
-
-// JWT_SECRET = "Divyanshu"
-// FOLDER_NAME = "WebDev"
-
-// RAZORPAY_KEY = rzp_test_O7Xs6kPoFjJYlI
-// RAZORPAY_SECRET = aSAFt2OmXXmysp6tkcUIvyJE
-
-// CLOUD_NAME = dxo2kr9bz
-// API_KEY = 354434363464364
-// API_SECRET = H6C6EfjUhDO0uj7D-QoFfikRUIY
-
-// MONGODB_URL = "mongodb+srv://pathakdivyanshu800:Kwy3374HgBTRBfAj@cluster0.lvmtpyh.mongodb.net/StudyNotionDB"
-// PORT = 4000
